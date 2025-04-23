1da5f3ad867580287be04b0da8301cd58c997dfa
Prince70008 <princekushwah7000@gmail.com>
2025-04-23 22:45:26 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,10 @@
+// Greet the user when the page loads
+window.onload = function () {
+    console.log("Welcome to ApexPlanet's official site!");
+    alert("Welcome to ApexPlanet! 🚀 Let's explore innovation together.");
+  };
+  
+   function contactUs() {
+    alert("Thanks for reaching out! 📬 We'll get back to you soon.");
+  }
+  
